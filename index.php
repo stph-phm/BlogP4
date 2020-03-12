f74201b2539b000651add0160d86e96d48111ac2
Stph phm <stephanie.pham.lnd@gmail.com>
2020-03-12 18:35:14 +0100

    structure générale de MVC

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+// Inclusion des fichiers principaux
+include_once '_config/config.php';
+include_once '_config/db.php';
+include_once '_functions/functions.php';
+
+// Définition de la page courante
+if (isset($_GET['page']) and !empty($_GET['page'])) {
+    // function trim pour ignorer les espaces dans le nom du fichier
+    // strtolower pour s'assurer que le nom du fichier est bien en miniscule
+    $page = trim(strtolower($_GET['page']));
+} else {
+    $page = 'home';
+}
+
+// Array contenant toutes les pages
+$allPages = scandir('controllers/');
+
+// Vérification de l'existance de la page
+if (in_array($page . '_controller.php', $allPages)) {
+
+  // Inclusion de la page
+    include_once 'models/' .$page. '_model.php';
+    include_once 'controllers/' .$page. '_controller.php';
+    include_once 'views/' .$page. '_view.php';
+} else {
+    // Retour d'une erreur
+    echo 'Erreur 404';
+}
---
+++ views/home_view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <?php include_once 'views/includes/head.php'?>
+
+    <title><?= ucfirst($page) ?></title>
+  </head>
+  <body>
+    <?php include_once 'views/includes/header.php'?>
+
+
+    <?php include_once 'views/includes/footer.php'?>
+    
+    <script src="assets/js/app.js"></script>
+  </body>
+</html>

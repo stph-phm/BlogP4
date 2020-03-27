21d6818eab34adc1d1417fa24e1f684097c2dc45
Stph phm <stephanie.pham.lnd@gmail.com>
2020-03-27 15:44:39 +0100

    page d'acueil - home (view et controller)

--- views/home_view.php
+++ views/home_view.php
@@ -2,15 +2,31 @@
 <html lang="fr" dir="ltr">
   <head>
     <?php include_once 'views/includes/head.php'?>
-
-    <title><?= ucfirst($page) ?></title>
   </head>
   <body>
-    <?php include_once 'views/includes/header.php'?>
+    <?php include_once 'views/includes/nav.php'?>
 
-      <h1>Bienvenue sur mon site </h1>
-    <?php include_once 'views/includes/footer.php'?>
-    
-    <script src="assets/js/app.js"></script>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4"><?= $lastArticle['title'] ?></h1>
+        <p class="lead"><?= substr($lastArticle['content'], 0, 100) ?> ...</p>
+        <a href="article?id=<?= $lastArticle['id'] ?>">Lire la suite</a>
+      </div>
+    </div>
+
+    <div class="container">
+    <div class="d-flex justify-content-around align-items-conter">
+      <?php foreach ($firstArticles as $index => $article): ?>
+      <div class="card col-5">
+        <div class="card-body">
+          <h5 class="card-title"><?= $article['title'] ?></h5>
+          <p class="card-text"><?= substr($article['content'], 0, 100) ?> ...</p>
+          <a href="article?id=<?= $article['id'] ?>" class="btn btn-link p-0">Lire la suite</a>
+        </div>
+      </div>
+      <?php endforeach; ?>
+    </div>
+  </div>
+      <?php include_once 'views/includes/footer.php' ?>
   </body>
 </html>

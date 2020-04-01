20b9cbe62b00ccaf880b6eb531976e5d96566c7c
Stph phm <stephanie.pham.lnd@gmail.com>
2020-04-01 13:53:49 +0200

    Mise en page Nav

--- views/includes/nav.php
+++ views/includes/nav.php
@@ -1,24 +1,35 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
   <a class="navbar-brand" href="home">Billet simple pour l'Alaska</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-
-      <?php foreach ($articles as $index => $articleLink): ?>
-      <li class="nav-item active">
-        <a class="nav-link" href="article?id=<?= $articleLink['id'] ?>"><?= $articleLink['title'] ?></a>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle nav-item active" href="#" id="navbarDropdown" role="button"
+          data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Tous les chapitres
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <?php foreach ($articles as $index => $articleLink): ?>
+          <a class="dropdown-item text-center"
+            href="article?id=<?= $articleLink['id']?>"><?= $articleLink['title']?></a>
+          <?php endforeach; ?>
+        </div>
       </li>
-        <?php endforeach; ?>
     </ul>
 
-  </div>
-  <?php if ($_SESSION) { ?>
+    <?php if ($_SESSION) { ?>
+
     <form class="form-inline my-2 my-lg-0" action="#" method="post">
-      <a class="btn btn-link btn-sm px-2" href="admin">Administration</a>
-      <button type="submit" class="btn btn-danger btn-sm" name="deconnexion">Se déconnecter</button>
+      <a class="btn btn-primary btn-sm px-2 mr-sm-2" href="admin">Administration</a>
+
+      <button type="submit" class="btn btn-danger btn-sm" name="deconnexion"><span><i class="fas fa-sign-out-alt"></i>
+        </span> Se déconnecter</button>
     </form>
-  <?php } ?>
+
+    <?php } ?>
+  </div>
 </nav>
